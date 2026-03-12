543024e4a6026f17b229f861281e8700e8edd0d0
titusgriebel <titus.griebel@gmx.de>
2026-03-12 23:11:27 +0100

    Initial commit, added convenience inference function

---
+++ code/__init__.py
@@ -0,0 +1,5 @@
+from he_segmentation.infer_on_image import infer_on_images
+
+__all__ = [
+    "infer_on_images"
+]
---
+++ code/he_segmentation/__init__.py
@@ -0,0 +1,5 @@
+from infer_on_image import infer_on_images
+
+__all__ = [
+    "infer_on_images"
+]
